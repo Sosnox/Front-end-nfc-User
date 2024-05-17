74b4c8d367f8a9218efd79380e2600eb03a45427
sosnox <intspxo@gmail.com>
2024-05-17 11:06:49 +0700

    com

--- nfc-app/api/Feedback/insertFeedback.tsx
+++ nfc-app/api/Feedback/insertFeedback.tsx
@@ -3,13 +3,11 @@
 const endpoint = process.env.NEXT_PUBLIC_ENDPOINT + '/post_feedback/';
 
 const sendDataToFastAPI = async (data: any): Promise<unknown> => {
-  console.log(data, "dataAAAA")
   const formData = new FormData();
   formData.append('id_boardgame', data.id_boardgame);
   formData.append('name_report', data.name_report);
   formData.append('detail_report', data.detail_report);
   formData.append('rating', data.rating);
-  formData.append('checktypes', data.checktypes);
   try {
     const response = await fetch(`${endpoint}`, {
       method: "POST",
