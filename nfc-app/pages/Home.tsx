b4a90517426d46da61a37377451a6b8c26e7bba3
PattaponAP <juniorpattapon@gmail.com>
2024-05-17 10:57:34 +0700

    Update Ui

--- nfc-app/pages/Home.tsx
+++ nfc-app/pages/Home.tsx
@@ -5,11 +5,11 @@ import AddGameCard from '@/components/addgame_menu';
 export default function HomePage() {
     return (
         <div>
-            <AddGameTopic></AddGameTopic>
+            <AddGameTopic/>
             <div className='flex w-screen justify-center text-xl font-medium mb-7'>
                 <h1>บอร์ดเกมอื่น ๆ</h1>
             </div>
-            <AddGameCard></AddGameCard>
+            <AddGameCard/>
         </div>
     );
     }
--- nfc-app/pages/_app.tsx
+++ nfc-app/pages/_app.tsx
@@ -4,11 +4,14 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Link from 'next/link'
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { Inter } from 'next/font/google'
+import { Kanit } from 'next/font/google'
 import PopupIcon from '@/components/popup_icon'
 
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Kanit({ 
+  subsets: ['latin'],
+  weight: ['400']
+  })
 export default function App({ Component, pageProps }: AppProps) {
   return (
 
